105849149557e129243a4380a817b2edadd7beed
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2019-09-05 00:16:39 -0300

    Correcting algorithm finalization

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ RECTANGLE_THICKNESS = 8
 WAIT_SELECTION_SORT = 5
 WAIT_INSERTION_SORT = 80
 WAIT_BUBBLE_SORT = 50
-WAIT_SHELL_SORT = 50
+WAIT_SHELL_SORT = 100
 
 def text(background, message, color, size, coordinate_x, coordinate_y):
     font = pygame.font.SysFont(None, size)
@@ -249,25 +249,43 @@ def shell_sort(rectangles, background):
         for i in range(gap, len(rectangles.set_rectangles)):
             if(gap == 1):
                 last = True
+                #pinta o primeiro retangulo
+                rectangles.set_rectangles[0].update_rectangle_in_screen_animation(GREEN, background, WAIT_SHELL_SORT)
             else:
                 last = False
 
             # pinta o rectangles.set_rectangles[i] de vermelho
             rectangles.set_rectangles[i].update_rectangle_in_screen_animation(RED, background, WAIT_SHELL_SORT)
-            temp = rectangles.set_rectangles[i]
             j = i
             # comeca do gap e verifica se os elementos dentro do gap dele para tras estao ordenados
             # rectangles.set_rectangles[j - gap].update_rectangle_in_screen_animation(BLUE, background, WAIT_SHELL_SORT)
-            while j >= gap and(rectangles.set_rectangles[j - gap].height > temp.height):
-                rectangles.set_rectangles[j].update_rectangle_in_screen_animation(RED, background, WAIT_SHELL_SORT)
-                # pinta rectangles.set_rectangles[j - gap] de azul  
-                rectangles.set_rectangles[j - gap].update_rectangle_in_screen_animation(BLUE, background, WAIT_SHELL_SORT)
+            while j >= gap:
+                if(rectangles.set_rectangles[j - gap].height > rectangles.set_rectangles[j].height):
+                    temp = rectangles.set_rectangles[j]
 
-                rectangles.set_rectangles[j].update_rectangle_in_screen_animation(WHITE, background, WAIT_SHELL_SORT)
+                    #pinta o ultimo azul do loop de branco
+                    rectangles.set_rectangles[j - gap -1].update_rectangle_in_screen_animation(WHITE, background, WAIT_SHELL_SORT)
+                    #pinta o vermelho anterior de branco
+                    rectangles.set_rectangles[j].update_rectangle_in_screen_animation(WHITE, background, WAIT_SHELL_SORT)
+                    
+                    #pinta o retangulo a ser trocado
+                    rectangles.set_rectangles[j - gap].update_rectangle_in_screen_animation(BLUE, background, WAIT_SHELL_SORT)
 
-                rectangles.set_rectangles[j] = rectangles.set_rectangles[j - gap]
+                    #troca o ret da pos j com o da pos j-1
+                    rectangles.set_rectangles[j] = rectangles.set_rectangles[j - gap]
+                    rectangles.set_rectangles[j - gap] = temp              
 
-                rectangles.set_rectangles[j].color = WHITE
+                    rectangles.set_rectangles[j].color = WHITE
+
+                    rectangles.update_set_rectangles_in_screen_animation(background, WAIT_SHELL_SORT)
+                    if(last):
+                        rectangles.set_rectangles[j].color = GREEN
+                else:
+                    if(last):
+                        rectangles.set_rectangles[j].color = GREEN
+                    else:                  
+                        break
+                j -= gap               
 
                 rectangles.update_set_rectangles_in_screen_animation(background, WAIT_SHELL_SORT)
             
